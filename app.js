07a2cbbb0de109275e4a7f1821c55cee073435cd
Agung Syon <ahmatagung645@gmail.com>
2023-08-31 15:01:31 +0700

    login form - add product

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const router = require('./routes');
 const app = express()
+const router = require('./routes');
+const session = require('express-session')
 const port = 3000
 
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: 'cuma aku yang tau',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false,
+    sameSite: true //for secure from csrf attack
+  }
+}))
 
 app.use(router)
 
